8e51d97e30699ce8be1f52236361c95dc2934c0f
Sagenthave <125151569+Sagenthave@users.noreply.github.com>
2023-04-13 21:58:21 -0400

    created a js file that allowed the times to be displayed and allowed for data to be stored in the local storage.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # work-day-scheduler
+
+# Objective 
+- Create a schedule that displays both the day and time, and allows user to write tasks according to time-blocks. 
+
+## Screeshot
+
+## Resources 
+- https://api.jquery.com/text/#:~:text=To%20set%20or%20get%20the,html()%20method.
+- https://day.js.org/docs/en/display/format
+- https://api.jquery.com/siblings/
+- https://stackoverflow.com/questions/12481439/jquery-this-keyword
+
+# Deployment 
---
+++ script.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+
+
+
+  var currentDay= $('#currentDay') 
+  currentDay.text(dayjs().format('dddd MMMM DD YYYY'));
+  // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+  // the code isn't run until the browser has finished rendering all the elements
+  // in the html.
+
+  for (var i = 9; i <= 16; i++) {
+    var time = $(
+      `<div id="hour${i}" class="row time-block present">
+      <div class="col-2 col-md-1 hour text-center py-3">${i}:00</div>
+      <textarea id="textarea" class="textvalue col-8 col-md-10 description" rows="3"> </textarea>
+      <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+        <i class="fas fa-save" aria-hidden="true"></i>
+      </button>
+    </div>`
+    );
+      $('#container').append(time);
+  }
+
+  $(document).on({ "click": function () {
+      const hour = $(this).siblings(".hour").text();
+      const text = $(this).siblings(".textvalue").val();
+      localStorage.setItem(hour, text);
+    }},
+    '.saveBtn')
+
+  // $('.saveBtn').on('click', function () {
+  //   console.log('hello')
+  //   var text = $(this).siblings('.description').val()
+  //   console.log(text)
+  //   var hour = $(this).parent('.col-2 col-md-1 hour text-center py-3').text();
+  //   localStorage.setItem(hour,text);
+
+  //   // TODO: Add a listener for click events on the save button. This code should
+  //   // use the id in the containing time-block as a key to save the user input in
+  //   // local storage. HINT: What does `this` reference in the click listener
+  //   // function? How can DOM traversal be used to get the "hour-x" id of the
+  //   // time-block containing the button that was clicked? How might the id be
+  //   // useful when saving the description in local storage?
+    
+  //   //
+  //   // TODO: Add code to apply the past, present, or future class to each time
+  //   // block by comparing the id to the current hour. HINTS: How can the id
+  //   // attribute of each time-block be used to conditionally add or remove the
+  //   // past, present, and future classes? How can Day.js be used to get the
+  //   // current hour in 24-hour time?
+  //   //
+  //   // TODO: Add code to get any user input that was saved in localStorage and set
+  //   // the values of the corresponding textarea elements. HINT: How can the id
+  //   // attribute of each time-block be used to do this?
+  //   //
+  //   // TODO: Add code to display the current date in the header of the page.
+  //});
+
+  function getFunction () {
+    $('.col-2 col-md-1 hour text-center py-3').each(function(){
+      var hour = $(this).text();
+      var text = localStorage.getItem(hour);
+
+      if(text !== null) {
+        $(this).siblings('.description').val()
+      }
+    })
+
+    
+  }
+  getFunction();
+
+
+});
